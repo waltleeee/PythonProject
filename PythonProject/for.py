66e273128c95de35b87522e763c0753062d2dc0b
李驊珈 <>
2019-05-16 12:48:26 +0800

    add for.py

---
+++ PythonProject/for.py
@@ -0,0 +1,18 @@
+arr1=[2,4,6,8,10]
+str1="hello python"
+
+for i in range(10):
+    print(i)
+print("***\n")    
+
+for i in range(len(arr1)):    
+    print(arr1[i])
+print("***2\n")    
+
+for i in arr1:
+    print(i)
+print("***3\n")    
+
+for i in str1:
+    print(i)
+print("***4\n")        
