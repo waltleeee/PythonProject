20656feb8f950485d1ae248ad9659ceef0be9711
李驊珈 <>
2019-05-22 15:24:56 +0800

    modify URL

--- PythonProject/mysite/mysite/urls.py
+++ PythonProject/mysite/mysite/urls.py
@@ -18,6 +18,6 @@ from django.contrib import admin
 from django.urls import path
 
 urlpatterns = [
-    url(r'^polls/', include('polls.urls')),
+    url(r'', include('polls.urls')),
     path('admin/', admin.site.urls),
 ]
